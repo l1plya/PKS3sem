8931ced002e20d1a089d46f34d82dba086e42533
l1plya <representic@gmail.com>
2024-11-19 19:52:03 +0300

    Проект создан

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Липорт Кирилл Алексеевич\nЭФБО-03-22',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
-          ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Авторизация', style: TextStyle(fontSize: 24)),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Логин',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Пароль',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            Row(
+              children: [
+                Checkbox(value: false, onChanged: (value) {}),
+                Text("Запомнить меня"),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Войти', style: TextStyle(fontSize: 18)),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(vertical: 15),
+              ),
+            ),
+            SizedBox(height: 10),
+
+
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Регистрация', style: TextStyle(fontSize: 18)),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.blue,
+                padding: EdgeInsets.symmetric(vertical: 15),
+                side: BorderSide(color: Colors.blue),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Восстановить пароль',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+            ),
+          ],
         ),
       ),
     );
